bf073cb4dc3f56be2d639077706e3aa7c9d14e97
Jeff <jeffreysabol345@gmail.com>
2025-07-16 00:21:08 -0400

    Add dynamic formatting for the PreLevel screen to work across all screen sizes/devices

--- Menus/PreLevel.gd
+++ Menus/PreLevel.gd
@@ -8,17 +8,11 @@ enum powerup {NONE, BURGER, ICECREAM, WINGS}
 
 # TODO do or don't let the player buy power ups on level 1? and hide it add?
 func _ready():
-	$ColorRect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	
 	get_tree().queue_delete($Selector)
 	get_tree().queue_delete($HBoxContainer)
 	Global.has_collar = true
-	# Set the lives and level name labels
-	$Lives.text = str(Global.total_lives)
-	$LevelName.text = Global.get_level_name(Global.level_count)
-	$LevelName.set_anchors_and_offsets_preset(Control.PRESET_CENTER_TOP)
-	$LevelName.offset_top += 44
-	$LevelName.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	
+	setup_prelevel_ui()
 	
 	# Start a timer or wait for player input to proceed to the next level
 	$Timer.start()
@@ -27,10 +21,8 @@ func _ready():
 	set_powerup_price_display(get_powerup_prices())
 	
 	# Show powerups if level 2+
-	if Global.level_count > 1:
-		$HBoxContainer.show()
-		
-	$BoneCount.text = str(Global.total_bones)
+	#if Global.level_count > 1:
+		#$HBoxContainer.show()
 	
 	current_selection = powerup.NONE
 
@@ -106,6 +98,36 @@ func _input(event):
 
 func _on_timer_timeout():
 	Global.start_next_level()
+	
+func setup_prelevel_ui():
+	$ColorRect.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+# Set the lives and level name labels
+	$LevelName.text = Global.get_level_name(Global.level_count)
+	$LevelName.set_anchors_and_offsets_preset(Control.PRESET_CENTER_TOP)
+	$LevelName.offset_top += 44
+	$LevelName.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	
+	$x.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	$x.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	
+	$Lives.text = str(Global.total_lives)
+	$Lives.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	$Lives.offset_left = 125
+	$Lives.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	
+	$Dog.global_position = $x.get_global_transform().origin - Vector2(175, 48)
+	
+	$BoneX.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	$BoneX.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	$BoneX.offset_top += 150  # Push it below the previous row
+
+	$BoneCount.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
+	$BoneCount.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	$BoneCount.offset_left = 100
+	$BoneCount.offset_top += 150
+
+	$Bone.global_position = $BoneX.get_global_transform().origin - Vector2(190, 0)
+
 
 func get_powerup_prices():
 	var hamburger: int = Global.hamburger_price
